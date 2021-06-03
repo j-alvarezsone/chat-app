773b0229bc64fa1c605504ed4b936d781c075ef5
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-03 10:23:39 +0200

    added ChatSelect component

---
+++ src/components/ChatSelect.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ChatSelect = () => {
+  return (
+    <div className='middle-screen'>
+      <div className='alert-info'>
+        <hr />
+        <h3>Select one person</h3>
+        <span>To start a conversation</span>
+      </div>
+    </div>
+  );
+};
