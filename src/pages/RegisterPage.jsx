f7639e2fa2db8e444559d142dda1c4a0de31864c
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-07 08:39:33 +0200

    created RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,23 +1,77 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import Swal from 'sweetalert2';
 
 export const RegisterPage = () => {
+  const [form, setForm] = useState({
+    name: 'Alberto',
+    email: 'albert@gmail.com',
+    password: '123456',
+  });
+
+  const { name, email, password } = form;
+  const { register } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const msg = await register(name, email, password);
+    if (msg !== true) {
+      Swal.fire('Error', msg, 'error');
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const allOk = () => {
+    return name.length && email.length && password.length > 0;
+  };
+
   return (
-    <form className='login100-form validate-form flex-sb flex-w'>
+    <form className='login100-form validate-form flex-sb flex-w' onSubmit={handleSubmit}>
       <span className='login100-form-title mb-3'>Chat - Register</span>
 
       <div className='wrap-input100 validate-input mb-3'>
-        <input className='input100' type='text' name='name' placeholder='Name' />
+        <input
+          className='input100'
+          type='text'
+          name='name'
+          placeholder='Name'
+          value={name}
+          onChange={handleChange}
+        />
         <span className='focus-input100'></span>
       </div>
 
       <div className='wrap-input100 validate-input mb-3'>
-        <input className='input100' type='email' name='email' placeholder='Email' />
+        <input
+          className='input100'
+          type='email'
+          name='email'
+          placeholder='Email'
+          value={email}
+          onChange={handleChange}
+        />
         <span className='focus-input100'></span>
       </div>
 
       <div className='wrap-input100 validate-input mb-3'>
-        <input className='input100' type='password' name='password' placeholder='Password' />
+        <input
+          className='input100'
+          type='password'
+          name='password'
+          placeholder='Password'
+          value={password}
+          onChange={handleChange}
+        />
         <span className='focus-input100'></span>
       </div>
 
@@ -30,7 +84,9 @@ export const RegisterPage = () => {
       </div>
 
       <div className='container-login100-form-btn m-t-17'>
-        <button className='login100-form-btn'>Create Account</button>
+        <button className='login100-form-btn' type='submit' disabled={!allOk()}>
+          Create Account
+        </button>
       </div>
     </form>
   );
