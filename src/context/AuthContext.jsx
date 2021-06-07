88774de1519743f3d17ce54d7f640e7377667d78
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-07 08:40:24 +0200

    added register

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -33,7 +33,25 @@ export const AuthProvider = ({ children }) => {
     return resp.ok;
   };
 
-  const register = (name, email, password) => {};
+  const register = async (name, email, password) => {
+    const resp = await fetchWithoutToken('login/new', { name, email, password }, 'POST');
+
+    if (resp.ok) {
+      localStorage.setItem('token', resp.token);
+      const { user } = resp;
+
+      setAuth({
+        uid: user.uid,
+        checking: false,
+        logged: true,
+        name: user.name,
+        email: user.email,
+      });
+      return true;
+    }
+
+    return resp.msg;
+  };
 
   const verifyToken = useCallback(() => {}, []);
 
