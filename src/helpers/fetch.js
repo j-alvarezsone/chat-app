93cb3a6304f10f0e2c156ecca45081a2ab685f05
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-07 09:06:29 +0200

    created fetchWithToken

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -18,3 +18,28 @@ export const fetchWithoutToken = async (endpoint, data, method = 'GET') => {
     return await resp.json();
   }
 };
+
+export const fetchWithToken = async (endpoint, data, method = 'GET') => {
+  const url = `${baseURL}/${endpoint}`;
+  const token = localStorage.getItem('token') || undefined;
+
+  if (method === 'GET') {
+    const resp = await fetch(url, {
+      headers: {
+        'x-token': token,
+      },
+    });
+    return await resp.json();
+  } else {
+    const resp = await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'x-token': token,
+      },
+      body: JSON.stringify(data),
+    });
+
+    return await resp.json();
+  }
+};
