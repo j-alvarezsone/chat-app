3165b77ef0074d004b1bd6a3d987324775e1bc56
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 19:22:35 +0200

    added handleClick with dispatch

--- src/components/SidebarChatItem.jsx
+++ src/components/SidebarChatItem.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ChatContext } from '../context/chat/ChatContext';
+import { types } from '../types/types';
 export const SidebarChatItem = ({ user }) => {
-  const { name, online } = user;
+  const { name, online, uid } = user;
+  const {
+    chatState: { activeChat },
+    dispatch,
+  } = useContext(ChatContext);
+
+  const handleClick = () => {
+    dispatch({
+      type: types.activateChat,
+      payload: uid,
+    });
+  };
 
   return (
-    <div className='chat_list'>
+    <div className={`chat_list ${uid === activeChat && 'active_chat'} `} onClick={handleClick}>
       <div className='chat_people'>
         <div className='chat_img'>
           <img src='/assets/user.png' alt='sunil' width='40px' height='40px' />
