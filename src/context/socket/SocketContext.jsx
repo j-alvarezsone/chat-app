3e8b9151e1d08b572b452ec98808cda7c3cab674
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 08:53:33 +0200

    added useSocket and verify logged user

---
+++ src/context/socket/SocketContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useEffect } from 'react';
+import { useSocket } from '../../hooks/useSocket';
+import { AuthContext } from '../auth/AuthContext';
+
+export const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+  const { socket, online, connectSocket, disconnectSocket } = useSocket('http://localhost:8080');
+  const { auth } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (auth.logged) {
+      connectSocket();
+    }
+  }, [auth, connectSocket]);
+
+  useEffect(() => {
+    if (!auth.logged) {
+      disconnectSocket();
+    }
+  }, [auth, disconnectSocket]);
+
+  return <SocketContext.Provider value={{ socket, online }}>{children}</SocketContext.Provider>;
+};
