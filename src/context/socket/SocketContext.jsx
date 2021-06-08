4f7d6cdcc0fc934959f68fad62071679fb1492f1
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 10:44:55 +0200

    added useEffect to save the connected users into the state

--- src/context/socket/SocketContext.jsx
+++ src/context/socket/SocketContext.jsx
@@ -1,12 +1,16 @@
 import { createContext, useContext, useEffect } from 'react';
 import { useSocket } from '../../hooks/useSocket';
 import { AuthContext } from '../auth/AuthContext';
+import { ChatContext } from '../chat/ChatContext';
+
+import { types } from '../../types/types';
 
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
   const { socket, online, connectSocket, disconnectSocket } = useSocket('http://localhost:8080');
   const { auth } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     if (auth.logged) {
@@ -19,6 +23,16 @@ export const SocketProvider = ({ children }) => {
       disconnectSocket();
     }
   }, [auth, disconnectSocket]);
+
+  // escuchar los cambios de los usuarios conectados
+  useEffect(() => {
+    socket?.on('users-list', (users) => {
+      dispatch({
+        type: types.usersLoaded,
+        payload: users,
+      });
+    });
+  }, [socket, dispatch]);
 
   return <SocketContext.Provider value={{ socket, online }}>{children}</SocketContext.Provider>;
 };
