3362cd3995064ae262582498fbbb6581e98aec2e
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 10:42:25 +0200

    added ChatProvider

--- src/ChatApp.jsx
+++ src/ChatApp.jsx
@@ -1,13 +1,16 @@
 import { AuthProvider } from './context/auth/AuthContext';
 import { SocketProvider } from './context/socket/SocketContext';
+import { ChatProvider } from './context/chat/ChatContext';
 import { AppRouter } from './router/AppRouter';
 
 export const ChatApp = () => {
   return (
-    <AuthProvider>
-      <SocketProvider>
-        <AppRouter />
-      </SocketProvider>
-    </AuthProvider>
+    <ChatProvider>
+      <AuthProvider>
+        <SocketProvider>
+          <AppRouter />
+        </SocketProvider>
+      </AuthProvider>
+    </ChatProvider>
   );
 };
