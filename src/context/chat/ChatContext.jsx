c5fa24198816a717ee35fd2088e58cc9e644e3ca
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 10:43:16 +0200

    created ChatContext and passed useReducer to the value

---
+++ src/context/chat/ChatContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useReducer } from 'react';
+import { chatReducer } from './chatReducer';
+
+export const ChatContext = createContext();
+
+const initialState = {
+  uid: '',
+  charActive: null, // UID del usuario al que yo quiero enviar mensajes
+  users: [],
+  messages: [], // El chat seleccionado
+};
+
+export const ChatProvider = ({ children }) => {
+  const [chatState, dispatch] = useReducer(chatReducer, initialState);
+  return <ChatContext.Provider value={{ chatState, dispatch }}>{children}</ChatContext.Provider>;
+};
