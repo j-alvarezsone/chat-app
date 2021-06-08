770943c94cdcf7f646cdb9cc4ecd4f6a7fe84106
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 18:57:23 +0200

    mapped users dynamically

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/auth/AuthContext';
+import { ChatContext } from '../context/chat/ChatContext';
 import { SidebarChatItem } from './SidebarChatItem';
+
 export const Sidebar = () => {
-  const chats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const {
+    chatState: { users },
+  } = useContext(ChatContext);
+
+  const {
+    auth: { uid },
+  } = useContext(AuthContext);
+
   return (
     <div className='inbox_chat'>
-      {chats.map((chat) => (
-        <SidebarChatItem key={chat} />
-      ))}
+      {users
+        .filter((user) => user.uid !== uid)
+        .map((user) => (
+          <SidebarChatItem key={user.uid} user={user} />
+        ))}
 
       {/* <!-- Espacio extra para scroll --> */}
       <div className='extra_space'></div>
