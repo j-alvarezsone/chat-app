429bf4b882f14d2db4cd2cbf007a8b478e434fd3
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 18:57:55 +0200

    added users name and online dynamically

--- src/components/SidebarChatItem.jsx
+++ src/components/SidebarChatItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-export const SidebarChatItem = () => {
+export const SidebarChatItem = ({ user }) => {
+  const { name, online } = user;
+
   return (
     <div className='chat_list'>
       <div className='chat_people'>
@@ -7,9 +9,12 @@ export const SidebarChatItem = () => {
           <img src='/assets/user.png' alt='sunil' width='40px' height='40px' />
         </div>
         <div className='chat_ib'>
-          <h5>Some random name</h5>
-          <span className='text-success'>Online</span>
-          <span className='text-danger'>Offline</span>
+          <h5>{name}</h5>
+          {online ? (
+            <span className='text-success'>Online</span>
+          ) : (
+            <span className='text-danger'>Offline</span>
+          )}
         </div>
       </div>
     </div>
