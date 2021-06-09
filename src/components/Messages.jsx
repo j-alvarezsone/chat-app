ebb9a9ae1a63c13c9dca0ee5c882344522e18109
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-09 12:47:25 +0200

    mapped messages from state

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SendMessage } from '../components/SendMessage';
 import { IncomingMessage } from '../components/IncomingMessage';
 import { OutgoingMessage } from '../components/OutgoingMessage';
+import { ChatContext } from '../context/chat/ChatContext';
+import { AuthContext } from '../context/auth/AuthContext';
 
 export const Messages = () => {
-  const msgs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const {
+    chatState: { messages },
+  } = useContext(ChatContext);
+
+  const {
+    auth: { uid },
+  } = useContext(AuthContext);
+
   return (
     <div className='mesgs'>
-      {/* <!-- Historia inicio --> */}
       <div className='msg_history'>
-        {msgs.map((msg) => (msg % 2 ? <IncomingMessage key={msg} /> : <OutgoingMessage key={msg} />))}
+        {messages.map((msg) =>
+          msg.to === uid ? (
+            <IncomingMessage key={msg._id} msg={msg} />
+          ) : (
+            <OutgoingMessage key={msg._id} msg={msg} />
+          ),
+        )}
       </div>
-      {/* <!-- Historia Fin --> */}
-
       <SendMessage />
     </div>
   );
