c15fc28ce59d3d331a0da524152cc54e83792254
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-09 12:45:30 +0200

    added newMessage

--- src/context/chat/chatReducer.js
+++ src/context/chat/chatReducer.js
@@ -9,12 +9,23 @@ export const chatReducer = (state, action) => {
       };
     case types.activateChat:
       if (state.activeChat === action.payload) return state;
-      
+
       return {
         ...state,
         activeChat: action.payload,
         messages: [],
       };
+
+    case types.newMessage:
+      if (state.activeChat === action.payload.from || state.activeChat === action.payload.to) {
+        return {
+          ...state,
+          messages: [...state.messages, action.payload],
+        };
+      } else {
+        return state;
+      }
+
     default:
       return state;
   }
