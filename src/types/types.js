be40855592f5527ba63caad3d319e57840422861
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-09 13:21:50 +0200

    added loadMessages types

--- src/types/types.js
+++ src/types/types.js
@@ -2,4 +2,5 @@ export const types = {
   usersLoaded: '[chat] Users loaded',
   activateChat: '[chat] Activate chat',
   newMessage: '[chat] New message',
+  loadMessages: '[chat] Load messages',
 };
